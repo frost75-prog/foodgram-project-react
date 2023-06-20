c9d064be4488f849b2e8ba3ab7a36cef6e76d373
Kirill Morozov <i@kirill-morozov.ru>
2023-06-21 00:24:01 +0300

    modified:   backend/api/pagination.py

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -1,5 +1,7 @@
+from django.core import paginator
 from rest_framework.pagination import PageNumberPagination
 
 
-class CustomPaginator(PageNumberPagination):
+class CustomPagination(PageNumberPagination):
+    django_paginator_class = paginator.Paginator
     page_size_query_param = 'limit'
