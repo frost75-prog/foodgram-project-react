7a1829d47aba534b252f9bb16d5fc51f63a8ec87
Kirill Morozov <i@kirill-morozov.ru>
2023-07-06 01:26:51 +0300

    modified:   backend/foodgram/settings.py

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -117,9 +117,12 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticatedOrReadOnly",
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
     ],
-    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],  # noqa: E501
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.SearchFilter",
+    ],
     "DEFAULT_PAGINATION_CLASS": "api.pagination.CustomPagination",
     "PAGE_SIZE": 6,
     "SEARCH_PARAM": "name",
