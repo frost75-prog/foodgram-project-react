8b5defa4e4c246ec31ca9368c6d90c44cd001c43
Kirill Morozov <i@kirill-morozov.ru>
2023-07-08 22:10:33 +0300

    modified:   backend/api/users_views.py

--- backend/api/users_views.py
+++ backend/api/users_views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404
 from djoser.views import UserViewSet
 from rest_framework import status
 from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 from apps.users.models import Follow, User
@@ -16,6 +17,7 @@ class CustomUserViewSet(UserViewSet):
     queryset = User.objects.all()
     pagination_class = CustomPagination
     serializer_class = CustomUsersSerialiser
+    permission_classes = (AllowAny,)
 
     @action(detail=False, permission_classes=(IsAuthenticatedOrAdmin,))
     def subscriptions(self, request):
