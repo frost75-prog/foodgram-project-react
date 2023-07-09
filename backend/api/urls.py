d909a6fa4cdda001921e1aefb3ddf0ddd3361fd7
Kirill Morozov <i@kirill-morozov.ru>
2023-07-09 11:09:57 +0300

    modified:   backend/api/urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -14,6 +14,6 @@ router.register(r'ingredients', IngredientViewSet, basename='ingredients')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('', include('djoser.urls')),
-    path(r'auth/', include('djoser.urls.jwt')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
