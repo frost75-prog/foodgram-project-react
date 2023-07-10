ca0037d255d3f1ca57db2db3f94d35189a09cbd0
Kirill Morozov <i@kirill-morozov.ru>
2023-07-10 13:48:16 +0300

    modified:   backend/api/users_serializers.py

--- backend/api/users_serializers.py
+++ backend/api/users_serializers.py
@@ -30,15 +30,22 @@ class CustomUsersSerialiser(serializers.UserSerializer):
 
 
 class CustomUserCreateSerializer(serializers.UserCreateSerializer):
-
     class Meta:
         model = User
-        fields = (
-            'email',
-            'id',
-            'username',
-            'first_name',
-            'last_name',
-            'password'
-        )
-        write_only_fields = ('password',)
+        fields = ('email', 'id', 'username',
+                  'first_name', 'last_name', 'password')
+
+        extra_kwargs = {
+            'first_name': {'required': True, 'allow_blank': False},
+            'last_name': {'required': True, 'allow_blank': False},
+            'email': {'required': True, 'allow_blank': False},
+        }
+
+    def validate_username(self, obj):
+        invalid_usernames = ['me', 'set_password',
+                             'subscriptions', 'subscribe']
+        if self.initial_data.get('username') in invalid_usernames:
+            raise serializers.ValidationError(
+                {'username': 'Вы не можете использовать этот username.'}
+            )
+        return obj
